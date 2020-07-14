3e4be84b3d204e618dc90c259247cd75b26f9761
mestrada <mestradafite@gmail.com>
2020-07-14 17:17:58 +0200

    Ganancias.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-profile',
     templateUrl: './profile.component.html',
     styleUrls: ['./profile.component.scss']
 })
+
+
 
 export class ProfileComponent implements OnInit {
 
