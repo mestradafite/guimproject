577a79dacbe050fd095119bcb16a28a34a96ebdc
mestrada <mestradafite@gmail.com>
2020-07-16 20:27:30 +0200

    Profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -12,6 +12,19 @@ export class ProfileComponent implements OnInit {
 
     constructor() { }
 
+    socialNetworks: string[] = ["Instagram", "Youtube", "Twitter"];
+    sortOrdersIcons: string[] = ["fa fa-instagram", "fa fa-youtube", "fa fa-twitter"];
+    selectedSortOrder: string = this.socialNetworks[0];
+    selectedSortOrderIcon: string = this.sortOrdersIcons[0];
+    currDiv: string = this.socialNetworks[0];
+
     ngOnInit() {}
+
+    ChangeSortOrder(selectedSocialNetwork: string) { 
+        this.selectedSortOrder = selectedSocialNetwork;
+        this.selectedSortOrderIcon = "fa fa-" + selectedSocialNetwork.toLowerCase();
+        this.currDiv = selectedSocialNetwork;
+        
+    }
 
 }
